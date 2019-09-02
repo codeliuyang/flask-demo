d8cd7c7bf4d93d97ef53dcd2a79a3de546a922a5
liuyang <liuyang@yitongmed.com>
2019-09-02 18:25:28 +0800

    修改格式

--- apps/v1/api/city_api.py
+++ apps/v1/api/city_api.py
@@ -4,6 +4,7 @@ from apps.v1.models.city import City
 
 city_api = Blueprint('city_api', __name__)
 
+
 @city_api.route("/blueprint")
 def get_city():
     city = db.session.query(City).first()
--- apps/v1/api/city_resource.py
+++ apps/v1/api/city_resource.py
@@ -1,11 +1,27 @@
+'''这个模块的写法是使用flask_restful进行开发
+完全遵循RESTful里对于资源的概念
+'''
 from flask_restful import Resource
-
 from apps.v1.models.city import City
 from db_ext import db
 
 
 class CityResource(Resource):
+
     def get(self):
         # return {"aa": "123"}
         first_city = db.session.query(City).first()
         return {"cityname": first_city.name}
+
+    def put(self):
+        # put操作方法
+        return ""
+
+    def post(self):
+        # post操作
+        return ""
+
+    def delete(self):
+        # delete操作
+        return ""
+
